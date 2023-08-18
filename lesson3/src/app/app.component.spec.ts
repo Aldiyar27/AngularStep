a63543fb5201b6a77af80c6e9f187f6e5f95985b
Ашкеев Алдияр Русланович <@lcoast.itstep>
2023-08-18 16:05:48 +0600

    hw 3

--- lesson3/src/app/app.component.spec.ts
+++ lesson3/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lesson3'`, () => {
+  it(`should have as title 'Post_Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lesson3');
+    expect(app.title).toEqual('Post_Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('lesson3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Post_Angular app is running!');
   });
 });
